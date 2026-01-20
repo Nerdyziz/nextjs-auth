cd96108d3c0047945daa40b22ad63d39a6bd5b27
Aziz_53 <avcreations53@gmail.com>
2026-01-20 23:23:36 +0530

    homae page setting

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,17 @@
 "use client";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 export default function Home() {
-  const router = useRouter();
+  
   return (
     <>
-    {router.push("/signup")}
+    <div className="flex flex-col justify-center items-center min-h-screen">
+      <h1>Welcome to Next.js Auth App</h1>
+      <hr />
+      <p className="mt-4">Please <Link href="/login">login</Link> or <Link href="/signup">register</Link> to continue.</p>
+
+    </div>
     </>
   );
 }
