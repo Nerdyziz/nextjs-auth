af882a15782425533e77f170a4862998d499afa9
Aziz_53 <avcreations53@gmail.com>
2026-01-20 22:18:05 +0530

    minor fix

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,11 @@
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
   return (
     <>
+    router.push("/signup")
     </>
   );
 }
